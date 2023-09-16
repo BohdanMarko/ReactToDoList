b0b92445f73f18e11650168902ec43da788eb4ad
Bohdan Marko <bohdan.marko.228@gmail.com>
2023-09-16 15:53:18 +0300

    use State for item input

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import { useState } from "react"
 import "./styles.css"
 
 // React component can return only ONE element,
 // so we use fragment element (<></>) to return <form> + <h1>
 export default function App() {
+  const [newItem, setNewItem] = useState("Create React ToDo List")
+  
   return (
     <> 
       <form className="new-item-form">
         <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input type="text" id="item" />
+          <label htmlFor="item" >New Item</label>
+          <input 
+            value={newItem} 
+            onChange={event => setNewItem(event.target.value)} 
+            type="text" 
+            id="item"
+          />
         </div>
         <button className="btn">Add</button>
       </form>
