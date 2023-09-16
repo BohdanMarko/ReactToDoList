84217845abe51ef44798107d70773eeb496c40e8
Bohdan Marko <bohdan.marko.228@gmail.com>
2023-09-16 15:15:00 +0300

    implement static page view

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
 import "./styles.css"
 
+// React component can return only ONE element,
+// so we use fragment element (<></>) to return <form> + <h1>
 export default function App() {
-  return "Hello World!"
+  return (
+    <> 
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      
+      <h1 className="header">ToDo List</h1>
+      
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  )
 }
